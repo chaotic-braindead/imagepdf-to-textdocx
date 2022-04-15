78b062d26f43db7b14491aa2b94fd58601d84ee1
Rafael Torres <102578910+chaotic-braindead@users.noreply.github.com>
2022-04-15 15:29:36 +0800

    removed docxcompose module

--- imagepdf-to-textdocx/main.py
+++ imagepdf-to-textdocx/main.py
@@ -1,11 +1,12 @@
-from PIL import Image
+from PIL import Image as Img
 from pdf2image import convert_from_path
 import pytesseract as tess
 from docx import Document
 from docx.enum.style import WD_STYLE_TYPE
 from docx.shared import Pt
-from docxcompose.composer import Composer
 import os
+from tkinter import *
+from tkinter import filedialog
 
 #pytesseract path
 tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -14,11 +15,11 @@ file_list = []
 image_names = []
 
 # converts pdf to image 
-def pdf_to_image(pdfs, page_numbers):
+def pdf_to_image(pdfs):
     global name
+    global pages
     pages = convert_from_path(pdfs, 350)
     name = pdfs.split('\\')[-1].split('.')[0]
-
     i = 1
 
     for page in pages:
@@ -28,18 +29,18 @@ def pdf_to_image(pdfs, page_numbers):
         page.save(image_name, "JPEG")
         i = i+1  
         image_names.append(image_name)
-        image_to_text(image_name, page_numbers)
+        image_to_text(image_name)
 
 # converts image produced by previous function to readable text through pytesseract
-def image_to_text(imageName, page_numbers):
+def image_to_text(imageName):
     print('------------------------------------------')
     print('Converting image to text...')
-    img = Image.open(imageName)
+    img = Img.open(imageName)
     text = tess.image_to_string(img)
-    text_to_document(imageName, text, page_numbers)
+    text_to_document(imageName, text)
 
 # creates a new document from the text produced through python-docx module
-def text_to_document(imageName, text, page_numbers):
+def text_to_document(imageName, text):
     print('------------------------------------------')
     print('Creating document from text...')
     document = Document()
@@ -54,31 +55,32 @@ def text_to_document(imageName, text, page_numbers):
     
     # if the length of the list of file_names is not equal to the page numbers, 
     # do not proceed to next function
-    if len(file_list) == int(page_numbers):
-        combine_all_docx(file_list)
+    if len(file_list) == len(pages):
+        combine_word_document(file_list)
 
-# merges all documents into one file through docxcompose module
-def combine_all_docx(files_list):
-    print('------------------------------------------')
-    print('Combining produced documents...')
-    number_of_sections=len(files_list)
-    master = Document(f'{files_list[0]}')
-    composer = Composer(master)
-    for i in range(1, number_of_sections):
-        doc_temp = Document(files_list[i])
-        composer.append(doc_temp)
-    composer.save(f"{name}.docx")
-    delete_jpg_and_pdf(files_list, number_of_sections)
+def combine_word_document(files_list):
+    merged_document = Document()
+    for index, file in enumerate(files_list):
+        sub_doc = Document(file)
+        if index < len(files_list)-1:
+            sub_doc.add_page_break()
+
+        for element in sub_doc.element.body:
+            merged_document.element.body.append(element)
+    merged_document.save(f'{name}.docx')
+    delete_jpg_and_pdf(files_list)
+
 
 # deletes jpgs and pdfs created earlier
-def delete_jpg_and_pdf(files_list, number_of_sections):
+def delete_jpg_and_pdf(files_list):
     print('------------------------------------------')
     print('Deleting duplicates...')
-    for i in range(0, number_of_sections):
+    for i in range(len(files_list)):
         os.remove(files_list[i])
         os.remove(image_names[i])
     print('------------------------------------------')
     print('Task completed')
 
+
 if __name__ == '__main__':
-    pdf_to_image(input("   Image PDF to DOCX  \n-----------------------\nEnter Directory of File:  \n"), int(input('How many pages does it have?: ')))
+    pdf_to_image(input("   Image PDF to DOCX  \n-----------------------\nEnter Directory of File:  \n"))
