59fec4f9e30114e152dee9dcadabe0f47b6a2e25
chaotic-braindead <rafaelsebastian0908@gmail.com>
2022-04-19 15:26:58 +0800

    Update main.py

--- imagepdf-to-textdocx/main.py
+++ imagepdf-to-textdocx/main.py
@@ -1,5 +1,6 @@
 from PIL import Image as Img
 from pdf2image import convert_from_path
+import pdf2image
 import pytesseract as tess
 from docx import Document
 from docx.enum.style import WD_STYLE_TYPE
@@ -7,6 +8,8 @@ from docx.shared import Pt
 import os
 from tkinter import *
 from tkinter import filedialog
+import time
+import threading
 
 #pytesseract path
 tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -78,9 +81,64 @@ def delete_jpg_and_pdf(files_list):
     for i in range(len(files_list)):
         os.remove(files_list[i])
         os.remove(image_names[i])
+
+    files_list.clear()
+    image_names.clear()
     print('------------------------------------------')
     print('Task completed')
 
-
 if __name__ == '__main__':
-    pdf_to_image(input("   Image PDF to DOCX  \n-----------------------\nEnter Directory of File:  \n"))
+    root = Tk()
+    root.title('PDF to Selectable Text DOCX Converter')
+    root.iconbitmap(r'C:\Users\raf\Downloads\folder.ico')
+    root.geometry('500x200')
+    root.configure(bg='#4f4d4d')
+    my_text = Text(root, width=50, height=1, font=('Segoe', 10))
+    my_text.grid(row=6, column=8, sticky="nsew", padx=2, pady=2)
+    my_text.place(x=25, y=25)
+    # my_text.pack(pady=25)
+    my_text.configure(state='disabled',)
+
+    def getlabel(status):
+        if status == 'Converting...':
+            my_label.place(x=205, y=75)
+            my_label.config(text=status, bg='#ffffff')
+        elif status == 'Select a file to convert':
+            my_label.place(x=193, y=75)
+            my_label.config(text=status, bg='#ffffff')      
+        else:
+            my_label.place(x=157, y=75)
+            my_label.config(text=status, bg='#53fc8e')
+            
+
+    def bttn(width, height, x, y, text, bcolor, fcolor, cmd):
+
+        button = Button(root, width=width, height=height, text=text,
+        fg=bcolor, bg=fcolor, border=0, activeforeground=fcolor,
+        command=cmd, font=('Helvetica', 10, 'bold'))
+
+        button.place(x=x, y=y)
+
+    def getpdf():
+        my_text.configure(state='normal')
+        path = filedialog.askopenfilename(initialdir=r'C:\Users\raf\Downloads', title='Select a PDF file to convert', filetypes=(('pdf files','*.pdf'), ('All files', '*.*')))
+        my_text.delete(1.0, END)
+        my_text.insert('1.0', path)
+        my_text.configure(state='disabled')
+        getlabel('Select a file to convert')
+
+    def convert():
+        path = my_text.get('1.0', END).rstrip()
+        getlabel('Converting...')
+        pdf_to_image(path)
+        getlabel('Successfully converted .pdf to .docx!')
+    
+
+    bttn(10, 1, 395, 23, 'Browse', '#ffffff', '#050404', getpdf)
+    bttn(20, 2, 166, 120, 'Convert', '#ffffff', '#050404', convert)
+    
+    my_label = Label(root, font=('Segoe', 8), text='Select a file to convert')
+    my_label.place(x=193, y=75)
+
+    
+    root.mainloop()
